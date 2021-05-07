83e1fb6932b0d9dafe1e7ed5ae4bb3e377249e34
mvpatel <bestmvpatel@gmail.com>
2021-05-07 01:04:29 +0100

    Added DTO files

--- src/main/java/com/atm/dto/ATMDTO.java
+++ src/main/java/com/atm/dto/ATMDTO.java
@@ -1,4 +1,14 @@
 package com.atm.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Builder
 public class ATMDTO {
+    private BigDecimal balance = new BigDecimal("8000");
 }
--- src/main/java/com/atm/dto/CustomerDTO.java
+++ src/main/java/com/atm/dto/CustomerDTO.java
@@ -1,4 +1,18 @@
 package com.atm.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Builder
 public class CustomerDTO {
+    private String accountNumber;
+    private String pin;
+    private BigDecimal balance;
+    private BigDecimal overDraft;
+    private BigDecimal withdrawalBalance;
 }
